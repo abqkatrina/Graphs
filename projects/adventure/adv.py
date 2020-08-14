c6ba2fa0399f7481aece86a7d114594694f4605c
Katrina Hernandez <abqkatrina@gmail.com>
2020-08-14 11:31:27 -0600

    graphs III and sprint challenge

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -4,6 +4,7 @@ from world import World
 
 import random
 from ast import literal_eval
+from util import Stack, Queue
 
 # Load world
 world = World()
@@ -18,24 +19,97 @@ map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
+
+room_dict = {}
+for i in room_graph:
+    room_dict[i] = room_graph[i][1]
+# print(room_dict)
+def get_adjacent(room_id):
+    return room_dict[room_id]
+
 world.load_graph(room_graph)
 
 # Print an ASCII map
-world.print_rooms()
+# world.print_rooms()
 
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+backwards_path = [] 
+visited = {}
+opposite_directions = {'n': 's', 's': 'n', 'w': 'e', 'e': 'w'}
+visited[player.current_room.id] = player.current_room.get_exits()
+
+while len(visited) < len(room_graph)-1:
+    if player.current_room.id not in visited:
+        visited[player.current_room.id] = player.current_room.get_exits()
+        visited[player.current_room.id].remove(backwards_path[-1])
+
+    while len(visited[player.current_room.id]) == 0:
+        backup = backwards_path.pop()
+        traversal_path.append(backup)
+        player.travel(backup)
+
+    dir = visited[player.current_room.id].pop(0)
+    if getattr(player.current_room, f"{dir}_to").id not in visited:
+        backwards_path.append(opposite_directions[dir])
+        traversal_path.append(dir)
+        player.travel(dir)
+    # else:
+    #     traversal_path.append(dir)
+    #     player.travel(dir)
+    #     print(opposite_directions[dir])
+    #     visited[player.current_room.id].remove(opposite_directions[dir])
+    #     backwards_path = []
+        
+    # traversal_path.append(dir)
+    # player.travel(dir)
 
 
+
+# def dead_ended(r, lastroom):
+#     if lastroom != -1:
+#         for key in room_dict[r].keys():
+#             if room_dict[r][key] == lastroom:
+#                 return False
+#         return True
+
+# def get_paths():
+#     returnstack = Stack()
+#     s=Stack()
+#     s.push(player.current_room.id)
+#     visited = []
+#     shortpath = []
+#     lastroom = 0
+#     returnpoint = 0
+#     while s.size() > 0:
+#         r = s.pop()
+#         if dead_ended(r, lastroom):
+#             shortpath = [get_opposite(i) for i in shortpath if i]
+#             shortpath.reverse()
+#             traversal_path.extend(shortpath)
+#             shortpath = []
+#             lastroom = returnstack.pop()
+#         if r not in visited:
+#             visited.append(r)
+#             shortpath.append(get_direction(r, lastroom))
+#             traversal_path.append(get_direction(r, lastroom))
+#             for i in range(len(room_dict[r].keys()) - 2):
+#                 returnstack.push(r)
+#                 shortpath = []
+#             lastroom = r
+#             for neighbor in list(get_adjacent(r).values()):
+#                 s.push(neighbor)
+
+#     return visited
 
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
-
+print(traversal_path)
 for move in traversal_path:
     player.travel(move)
     visited_rooms.add(player.current_room)
@@ -51,12 +125,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,5 @@
+import random
+from util import Stack, Queue
 class User:
     def __init__(self, name):
         self.name = name
@@ -19,6 +21,28 @@ class SocialGraph:
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+
+    #  want a way to get friend ids
+    def get_friend(self, user_id):
+        return self.friendships[user_id]
+
+    # want a way to make a path of friendships (bfs)
+    def bfs(self, user_id, friend_id):
+        q = Queue()
+        q.enqueue([user_id])
+        visited = set()
+        while q.size() > 0:
+            p = q.dequeue()
+            last = p[-1]
+            if last == friend_id:
+                return p
+            if last not in visited:
+                visited.add(last)
+                for f in self.get_friend(last):
+                    copy = p.copy()
+                    copy.append(f)
+                    q.enqueue(copy)
+
 
     def add_user(self, name):
         """
@@ -42,11 +66,28 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
+        # placeholder array for shuffling
+        potentials = []
 
-        # Add users
+        # create new user
+        for x in range(num_users):
+            # use add method with user name as argument -- user_id in this case
+            self.add_user(f"User {x + 1}")
 
-        # Create friendships
+        # for each user id, add friend ids
+        for user in self.users:
+            for friend_id in range(user + 1, self.last_id + 1):
+                # add relationship object to list of friendships
+                potentials.append((user, friend_id))
+
+        # shuffle list of friendships
+        random.shuffle(potentials)
+
+        # for each user, add friendships with connections
+        for x in range(num_users * avg_friendships // 2):
+            # add random friendship to user -- use add method with edge to next user(friend)
+            friendship = potentials[x]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -58,9 +99,23 @@ class SocialGraph:
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        s = Stack()
+        s.push(user_id)
+        # while there's something in the stack, make current the popped item
+        while s.size() > 0:
+            v = s.pop()
+            # if current hasn't been used, mark used and 
+            # create a path between starting user and current
+            # using the visited list
+            if v not in visited:
+                path = self.bfs(user_id, v)
+                visited[v] = path
+                # for each friend of the current, add to stack and start again
+                for f in self.get_friend(v):
+                    s.push(f)
+        # when all connected user ids have been visited and the stack is empty, return the list
         return visited
-
+        # TODO first user/ unconnected users show path to self as self. Change to return nothing?
 
 if __name__ == '__main__':
     sg = SocialGraph()
